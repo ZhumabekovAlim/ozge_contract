f258420d532dc6b0326751f1b01f70eccc92d3cf
ZhumabekovAlim <alim.zhumabekov@gmail.com>
2025-03-02 17:13:43 +0500

    search for all

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -683,7 +683,7 @@ func (h *CompanyHandler) CheckPassword(w http.ResponseWriter, r *http.Request) {
 
 	// Отправляем результат
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]bool{"match": match})
+	json.NewEncoder(w).Encode([]bool{match})
 }
 
 type DiscardHandler struct {
